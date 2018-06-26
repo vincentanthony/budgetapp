25a4587763395bd122271db6d3201c334c66163f
vincemasciale <vince@lonesuspect.com>
2018-06-26 14:03:49 -0700

    second

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -4,8 +4,8 @@ import { Item } from './item.model';
 export class ItemService {
 
   private items: Item[] = [
-    new Item('Lone Suspect', 1, 'Allow', 1, 40000, 40000 ),
-    new Item('Mike Macari', 1, 'Allow', 1, 10000, 10000 ),
+    new Item('Producer 1', 1, 'Allow', 1, 40000, 40000 ),
+    new Item('Producer 2', 1, 'Allow', 1, 10000, 10000 ),
     ];
 
   getItems() {
